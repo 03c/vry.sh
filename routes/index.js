5d1a0091e4c880f5646855f2290efe113870553b
Chris Child <chris.child@gmail.com>
2020-11-28 16:02:42 +0000

    refactor db and hashids

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { URL } = require('../database');
-const hashids = require('../utils/hashids');
+const { URL } = require('../config/database');
+const hashids = require('../config/hashids');
 
 const router = express.Router();
 
